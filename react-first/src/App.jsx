fa921ab2ea9a3b9d5f4e983a73277e32cb5b2292
BenjaminCulverhouse <124332878+BenjaminCulverhouse@users.noreply.github.com>
2023-11-06 13:11:57 -0500

    Added bootstrap navbar component to allow for better navbar functionality.

--- react-first/src/App.jsx
+++ react-first/src/App.jsx
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react'
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
--- react-first/src/pages/Components/TopbarGeneral.jsx
+++ react-first/src/pages/Components/TopbarGeneral.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import {NavLink} from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const TopbarGeneral = ({signedIn}) => {
     const[IsOpen, setOpen] = useState(false);
@@ -14,23 +18,18 @@ const TopbarGeneral = ({signedIn}) => {
         linkItems.push({path: "./login", component:"Login/Sign Up", dropdown: false})
     }
 
-    return(
-        <div className="container">
-            <div className="top_bar">
-                {
-                    linkItems.map((item, index) => (
-                        <NavLink
-                            to={item.path}
-                            key={index}
-                            className="link"
-                        >
-                            <div className='link_text'>{item.component}</div>
-                        </NavLink>
-                    ))
-                }
-
-            </div>
-        </div>
-    );
+    return (
+        <Navbar data-bs-theme="dark" className="bg-body-tertiary">
+            <Navbar.Brand href="./">Grade-TrackVT</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="./">Home</Nav.Link>
+                <Nav.Link href="./about">About</Nav.Link>
+                <Nav.Link href="./faculty">Faculty</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+      );
 }
 export default TopbarGeneral;
