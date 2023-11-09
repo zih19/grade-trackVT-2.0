79fb5ad80d1e4db6b2c4ef4caab9652d80e6de55
BenC114 <95393901+BenC114@users.noreply.github.com>
2023-11-09 13:54:45 -0500

    Created a dropdown menu that allows the user to select the major that he or she is interested in seeing classes for.

--- react-first/src/pages/Components/TopbarSpecific.jsx
+++ react-first/src/pages/Components/TopbarSpecific.jsx
@@ -1,8 +1,26 @@
 import {Stack, AppBar, Toolbar, Typography, Button, IconButton} from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import VTlogo from '../../Assets/VTLogo.png';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 const TopBarSpec = ({signedIn}) => {
+
+    const majorsData = [
+        { id: 1, title: 'Aerospace Engineering', content: 'Click to learn more about the courses offered in the aerospace program.', coursesURL: '/Courses' },
+        { id: 2, title: 'Biological Systems Engineering', content: 'Click to learn more about the courses offered in the biological systems program.', coursesURL: '/Courses'  },
+        { id: 3, title: 'Biomedical Engineering', content: 'Click to learn more about the courses offered in the biomedical program.', coursesURL: '/Courses'  },
+        { id: 4, title: 'Building Construction', content: 'Click to learn more about the courses offered in the building construction program.', coursesURL: '/Courses'  },
+        { id: 5, title: 'Chemical Engineering', content: 'Click to learn more about the courses offered in the chemical engineering program.', coursesURL: '/Courses'  },
+        { id: 6, title: 'Civil Engineering', content: 'Click to learn more about the courses offered in the civil engineering program.', coursesURL: '/Courses'  },
+        { id: 7, title: 'Computer Engineering', content: 'Click to learn more about the courses offered in the computer engineering program.', coursesURL: '/Courses'  },
+        { id: 8, title: 'Computer Science', content: 'Click to learn more about the courses offered in the computer science program.', coursesURL: '/Courses'  },
+        { id: 9, title: 'Construction Engineering and Management', content: 'Click to learn more about the courses offered in the construction and engineering management program.', coursesURL: '/Courses'  },
+        { id: 10, title: 'Electrical Engineering', content: 'Click to learn more about the courses offered in the electrical engineering program.', coursesURL: '/Courses'  },
+        { id: 11, title: 'Industrial and Systems Engineering', content: 'Click to learn more about the courses offered in the industrial and systems engineering program.', coursesURL: '/Courses'  },
+        { id: 12, title: 'Materials Science Engineering', content: 'Click to learn more about the courses offered in the materials science program.', coursesURL: '/Courses'  },
+        { id: 13, title: 'Mechanical Engineering', content: 'Click to learn more about the courses offered in the mechanical engineering program.', coursesURL: '/Courses'  },
+        { id: 14, title: 'Mining Engineering', content: 'Click to learn more about the courses offered in the mining engineering program.', coursesURL: '/Courses'  }]
+
     const navigate = useNavigate();
 
     const handleHomeClick = () => {
@@ -13,13 +31,15 @@ const TopBarSpec = ({signedIn}) => {
          navigate('/About');
     }
 
-    const handleFacultyClick = () => {
-         navigate('/Faculty')     
+    const handleMajorClick = () => {
+         navigate('/Majors')     
     }
 
     const handleLoginRegisterClick = () => {
          navigate('/Login')
     }
+
+    //<Button color='inherit' onClick={handleMajorClick}> MajorsPage </Button>
     return (
         <>
             <AppBar position='static' color='transparent'>
@@ -37,7 +57,16 @@ const TopBarSpec = ({signedIn}) => {
                     <Stack spacing={2} direction='horizontal'>
                         <Button color='inherit' onClick={handleHomeClick}> Home </Button>
                         <Button color='inherit' onClick={handleAboutClick}> About </Button>
-                        <Button color='inherit' onClick={handleFacultyClick}> Faculty </Button>
+                        <Dropdown>
+                            <Dropdown.Toggle color='inherit' variant='inherit' id='dropdown-basic'>
+                                Majors
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                {majorsData.map((major) => (
+                                    <Dropdown.Item href={major.coursesURL}>{major.title}</Dropdown.Item>
+                                ))}
+                            </Dropdown.Menu>
+                        </Dropdown>
                         {!signedIn ? 
                         <Button color='inherit' onClick={handleLoginRegisterClick}> Login/Register </Button>
                         :null}
--- react-first/src/pages/Majors.jsx
+++ react-first/src/pages/Majors.jsx
@@ -10,19 +10,44 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import React from 'react'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'; 
+import Container from 'react-bootstrap/Container';
 
 
 const Majors = () => {
+
+  const majorsData = [
+    { id: 1, title: 'Aerospace Engineering', content: 'Click to learn more about the courses offered in the aerospace program.', coursesURL: '/Courses' },
+    { id: 2, title: 'Biological Systems Engineering', content: 'Click to learn more about the courses offered in the biological systems program.', coursesURL: '/Courses'  },
+    { id: 3, title: 'Biomedical Engineering', content: 'Click to learn more about the courses offered in the biomedical program.', coursesURL: '/Courses'  },
+    { id: 4, title: 'Building Construction', content: 'Click to learn more about the courses offered in the building construction program.', coursesURL: '/Courses'  },
+    { id: 5, title: 'Chemical Engineering', content: 'Click to learn more about the courses offered in the chemical engineering program.', coursesURL: '/Courses'  },
+    { id: 6, title: 'Civil Engineering', content: 'Click to learn more about the courses offered in the civil engineering program.', coursesURL: '/Courses'  },
+    { id: 7, title: 'Computer Engineering', content: 'Click to learn more about the courses offered in the computer engineering program.', coursesURL: '/Courses'  },
+    { id: 8, title: 'Computer Science', content: 'Click to learn more about the courses offered in the computer science program.', coursesURL: '/Courses'  },
+    { id: 9, title: 'Construction Engineering and Management', content: 'Click to learn more about the courses offered in the construction and engineering management program.', coursesURL: '/Courses'  },
+    { id: 10, title: 'Electrical Engineering', content: 'Click to learn more about the courses offered in the electrical engineering program.', coursesURL: '/Courses'  },
+    { id: 11, title: 'Industrial and Systems Engineering', content: 'Click to learn more about the courses offered in the industrial and systems engineering program.', coursesURL: '/Courses'  },
+    { id: 12, title: 'Materials Science Engineering', content: 'Click to learn more about the courses offered in the materials science program.', coursesURL: '/Courses'  },
+    { id: 13, title: 'Mechanical Engineering', content: 'Click to learn more about the courses offered in the mechanical engineering program.', coursesURL: '/Courses'  },
+    { id: 14, title: 'Mining Engineering', content: 'Click to learn more about the courses offered in the mining engineering program.', coursesURL: '/Courses'  }]
+
     return (
-        <Card style={{ width: '18rem' }}>
-          <Card.Body>
-            <Card.Title>Computer Science</Card.Title>
-            <Card.Text>
-              Browse the courses that are offered within the computer science program.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
+      <Container>
+      <h1>Majors</h1>
+      <Row>
+        {majorsData.map((major) => (
+          <Col lg={3} md={6} key={major.id}>
+            <Card style={{height: '15rem', width: '18rem'}}>
+              <Card.Title>{major.title}</Card.Title>
+              <Card.Text className='card-body d-flex flex-column' >{major.content}</Card.Text>
+              <Button variant="primary" href={major.coursesURL}>Learn More</Button>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+      </Container>
     );
 }
 
