0c5f833fccf158a17e8d7c45706da56d1570f019
BenjaminCulverhouse <124332878+BenjaminCulverhouse@users.noreply.github.com>
2023-12-03 13:30:54 -0500

    Added more pictures and links to the major info pages for majors that actually have tracks.

--- react-first/src/pages/MajorDescription.jsx
+++ react-first/src/pages/MajorDescription.jsx
@@ -140,18 +140,28 @@ const MajorDescription = () => {
       //https://www.vt.edu/academics/majors/building-construction.html https://mlsoc.vt.edu/programs/undergraduate/bachelor-of-science-in-building-construction.html
       content: [
         "The building construction department focuses on teaching its students the engineering behind construction projects from the constructing, managing, and performance of buildings to their sustainability. ",
-        "This focus on the different parts of this field allows for the students to gain a very diverse skill set that will be useful when working in future construction, no matter what role they may take.",
+        "This focus on the different parts of this field allows for the students to gain a very diverse skill set that will be useful when working in future construction, no matter what role they may take. ",
         "Students may also choose a degree track which allows them to attain a more specialized degree in this field."
       ],
+
+      //https://housing.com/news/hudco-housing-and-urban-development-corporation-limited/
+      //https://ismailsusanti.blogspot.com/2021/07/faculty-of-built-environment-were-here.html
+      //https://www.constructiontuts.com/sustainable-construction/
       imageURLs: [
         {
-
+          label: "Emergent Technologies for Construction, Housing and Development",
+          labelLink: "https://mlsoc.vt.edu/programs/undergraduate/bachelor-of-science-in-building-construction.html",
+          imagelink: "https://assets-news.housing.com/news/wp-content/uploads/2021/02/24165258/All-you-need-to-know-about-Housing-and-Urban-Development-Corporation-HUDCO-FB-1200x700-compressed.jpg"
         },
         {
-
+          label: "Information Systems in Built Environment",
+          labelLink: "https://mlsoc.vt.edu/programs/undergraduate/bachelor-of-science-in-building-construction.html",
+          imagelink: "https://knowledge.uli.org/-/media/images/reports/decarbonizing-the-built-environment.jpg?h=726&mw=1290&w=1290&rev=ed3d9cbab0554fbabd9dbd056352ba46&hash=4A0F7CC693EC49171B5F87D85CD2AC32"
         },
         {
-
+          label: "Sustainable Building Performance",
+          labelLink: "https://mlsoc.vt.edu/programs/undergraduate/bachelor-of-science-in-building-construction.html",
+          imagelink: "https://www.constructiontuts.com/wp-content/uploads/2019/12/sustainable-strategy-in-building.jpg"
         }
       ], 
       courseURL: "/majordescription/BC"
@@ -161,29 +171,86 @@ const MajorDescription = () => {
       //https://eng.vt.edu/academics/undergraduate-students/explore-engineering/chemical-engineering.html
       content: [
         "Students within the chemical engineering program will take a variety of courses that help to teach them how to apply a range of skills to numerous fields in everyday life. ",
-        "Students take a variety of courses with a focus on chemistry, math, physics, and biochemistry. Additionally, students will be introduced to challenging but rewarding labs that help them get practical hands-on knowledge of their field.",
-        "There are three optional tracks in this major that a student can choose to focus on. If chosen, the tracks restrict the courses a student will take to allow them a more focused overall degree goal."
+        "Students take courses with a focus on chemistry, math, physics, and biochemistry. Additionally, students will be introduced to challenging but rewarding labs that help them get practical hands-on knowledge of their field. ",
+        "There are three tracks in this major that a student can choose to focus on. When chosen, the tracks restrict the courses a student will take to allow them a more focused overall degree goal."
       ],
-      imageURLs: [], 
+      //https://www.c2es.org/2022/07/the-time-for-climate-action-is-now/
+      //https://www.world-today-news.com/definition-data-science-futura-tech/
+      //https://www.healthworkscollective.com/healthcare-tech-advances-clinics-use/
+      imageURLs: [
+      {
+        label: "Climate and energy solutions",
+        labelLink: "https://che.vt.edu/programs/undergraduate-program.html",
+        imagelink: "https://www.c2es.org/wp-content/uploads/2022/07/iStock-1306849663-scaled.jpg"
+      },
+      {
+        label: "Computational and data sciences",
+        labelLink: "https://che.vt.edu/programs/undergraduate-program.html",
+        imagelink: "https://cdn.futura-sciences.com/buildsv6/images/wide1920/9/5/0/950321c11d_50173256_data-science-1.jpg"
+      },
+      {
+        label: "Healthcare technologies",
+        labelLink: "https://che.vt.edu/programs/undergraduate-program.html",
+        imagelink: "https://www.healthworkscollective.com/wp-content/uploads/2017/08/healthcare-tech.jpg"
+      }], 
       courseURL: "/majordescription/CHE"
     },
     { id: 'CEE', 
-      title: "Civil Engineering",
+      title: "Civil/Environmental Engineering",
       //https://cee.vt.edu/AboutUs-menu/fast_facts.html
       content: [
-        "The civil engineering department strives to provide its students with a wide array of skills that are expected of an engineer in this field. Covering a broad area of instruction, skills developed include critical thinking, problem solving, communication, and leadership. ",
-        "Civil engineering students will have a coursework that will focus on mathematics and mechanics in the first two years, while moving to more specialized and specific courses in the third and fourth years of coursework.",
+        "The civil/environmental engineering department strives to provide its students with a wide array of skills that are expected of an engineer in this field. Covering a broad area of instruction, skills developed include critical thinking, problem solving, communication, and leadership. ",
+        "Civil engineering students will have a coursework that will focus on mathematics and mechanics in the first two years, while moving to more specialized and specific courses in the third and fourth years of coursework. ",
         "There are also specializations that students can choose to give their degree a central focus."
       ],
+      //https://www.handle.com/construction-project-management/
+      //https://us.anteagroup.com/news-events/blog/environmental-engineers-week
+      //https://www.riley-group.com/expertise/geotechnical-engineering/
+      //https://nearterm.com/are-there-great-career-opportunities-in-the-land-development-engineering-field/
+      //https://www.tuf-bar.com/composite-materials-for-civil-engineering-applications/
+      //https://carrollengineering.com/services/structural-engineering-services/
+      //http://martinjoyce10.blogspot.com/2013/02/civil-engineering-in-nutshell.html
+      //https://www.straughanenvironmental.com/markets-services/services/water-resources-and-civil-engineering/
       imageURLs: [
         {
-
+          label: "Construction Engineering and Management",
+          imagelink: "https://www.handle.com/wp-content/uploads/2020/07/construction-project-management-scaled.jpeg",
+          labelLink: "https://cee.vt.edu/Undergraduate-menu/prospective-undergraduate-students.html"
         },
         {
-
+          label: "Environmental",
+          imagelink: "https://cdnpreprodanteagroup.blob.core.windows.net/prod/media/resized/3/7d8b1a01-0e7f-4b5a-bf0a-b2dd3465a9b4/autox1160_gettyimages-814023320.jpg",
+          labelLink: "https://cee.vt.edu/Undergraduate-menu/prospective-undergraduate-students.html"
         },
         {
-
+          label: "Geotechnical",
+          imagelink: "https://www.riley-group.com/wp-content/uploads/2018/02/Geotechnical-Engineering.jpg",
+          labelLink: "https://cee.vt.edu/Undergraduate-menu/prospective-undergraduate-students.html"
+        },
+        {
+          label: "Land Development",
+          imagelink: "https://nearterm.com/wp-content/uploads/2021/07/nearterm-civil-engineering-land-development.jpg",
+          labelLink: "https://cee.vt.edu/Undergraduate-menu/prospective-undergraduate-students.html"
+        },
+        {
+          label: "Materials",
+          imagelink: "https://www.tuf-bar.com/wp-content/uploads/2018/11/Civil-Engineering-Applications.jpg",
+          labelLink: "https://cee.vt.edu/Undergraduate-menu/prospective-undergraduate-students.html"
+        },
+        {
+          label: "Structural",
+          imagelink: "https://carrollengineering.com/wp-content/uploads/2021/01/Structural-Engineering_Compressed.jpg",
+          labelLink: "https://cee.vt.edu/Undergraduate-menu/prospective-undergraduate-students.html"
+        },
+        {
+          label: "Transportation",
+          imagelink: "https://3.bp.blogspot.com/-AqxczPx7QIU/UX8XgP2k0UI/AAAAAAAAACI/4Q8kQF5fPQk/s1600/transportation-Engineering.jpg",
+          labelLink: "https://cee.vt.edu/Undergraduate-menu/prospective-undergraduate-students.html"
+        },
+        {
+          label: "Water Resources",
+          imagelink: "https://www.straughanenvironmental.com/wp-content/uploads/2021/04/AdobeStock_125778651-lg-crop.jpg",
+          labelLink: "https://cee.vt.edu/Undergraduate-menu/prospective-undergraduate-students.html"
         }
       ], 
       courseURL: "/majordescription/CEE"
@@ -213,15 +280,24 @@ const MajorDescription = () => {
         "The computer science department strives to teach students a wide range of skills that allow for a better understanding of the profession as a whole as well as allowing the students to pick an area of computer science that is particularly interesting to them.",
         "The computer science department has three tracks that students can choose from, which are listed below."
       ],
+      //https://www.pixelstalk.net/hd-computer-science-backgrounds/
+      //https://betanews.com/2015/02/02/how-the-cloud-boosts-business-security/
+      //https://ebs-integrator.com/blog/basics-data-centric-vs-data-driven-the-whys-and-whats/
       imageURLs: [
         {
-
+          label: "Computer science",
+          imagelink: "https://www.pixelstalk.net/wp-content/uploads/2016/07/Computer-Science-Pictures-HD.jpg",
+          labelLink: "https://cs.vt.edu/Undergraduate.html"
         },
         {
-
+          label: "Secure computing",
+          imagelink: "https://betanews.com/wp-content/uploads/2015/02/Cloud-security-800x450.png",
+          labelLink: "https://cs.vt.edu/Undergraduate.html"
         },
         {
-            
+          label: "Data-centric computing",
+          imagelink: "https://ebs-integrator.com/wp-content/uploads/2020/08/DC-DD_Stats-3.png",
+          labelLink: "https://cs.vt.edu/Undergraduate.html"
         }
       ], 
       courseURL: "/majordescription/CS"
